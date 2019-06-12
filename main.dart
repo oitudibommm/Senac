c9e9c2537d5e57188593c3eeb0aee6ca319add89
Alana Anjo Costa Ferreira de Andrade <51175504+oitudibommm@users.noreply.github.com>
2019-06-12 15:26:18 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new Material(
+      color: Colors.pink,
+      child: new Center(
+        child: new Text("Hello World",
+          textDirection: TextDirection.ltr,
+          style: new TextStyle(fontSize: 40,
+              color: Colors.amberAccent,
+          ),
+        ),
+      )
+  )
+  ); // RunApp
+}
